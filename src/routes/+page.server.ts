30f8fb66fca5871c30a97b803627ff174894e3c2
martin kapp <martin@coderats.net>
2025-03-20 22:43:32 +0200

    Configurable NAME, updated README, lean docker image (#6)

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,6 +4,7 @@ export const load: PageServerLoad = ({ locals }) => {
 	return {
 		serverHost: locals.serverHost,
 		serverIP: locals.serverIP,
-		clientIP: locals.clientIP
+		clientIP: locals.clientIP,
+		name: process.env.NAME || 'Web'
 	};
 };
